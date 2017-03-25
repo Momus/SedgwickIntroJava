dbe3cd7a7de13674c89e991bac189303cb464c5c
Dmitri G. Brengauz <dmitri@momus.net>
2017-03-25 17:57:28 -0600

    Initial commit

---
+++ src/main/java/org/momus/App.java
@@ -0,0 +1,13 @@
+package org.momus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
